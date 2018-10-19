0f1ed36e7a7c4074a61637fff8aed44ae2b25654
David Moench <domoench@gmail.com>
2018-10-19 13:52:55 -0400

    Try to fix the weird shuffling problem.
    Issue: Images and names are getting paired incorrectly on the deployed
    version only (doesn't happen locally). Not really sure how it's
    happening yet.

--- src/components/creatorThumbList.js
+++ src/components/creatorThumbList.js
@@ -13,14 +13,14 @@ export default ({creators}) => {
             const link = "/" + creator.slug
             const image = creator.images[0]
             return (
-              <Link to={link} key={creator.id}>
-                <li>
+              <li key={creator.id}>
+                <Link to={link}>
                   <p style={{clear: `both`, padding: `1rem 0`}}>
                     {creator.name}
                     <img src={image.file.url} alt={creator.name} style={{maxWidth: `200px`, float: `right`, padding: `0 1rem`}}/>
                   </p>
-                </li>
-              </Link>
+                </Link>
+              </li>
             )
           })
         }
