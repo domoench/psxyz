a9b2720c8b2793c900507b01e85fddce7e86b87a
David Moench <domoench@gmail.com>
2018-10-24 21:40:35 -0400

    Randomize order at build time only, not in component logic.
    Add a random numerical ID to each creator in onCreateNode so you can
    sort in the graphql query.

--- src/components/creatorThumbList.js
+++ src/components/creatorThumbList.js
@@ -3,27 +3,25 @@ import { Link } from "gatsby"
 
 // A list of creators, with name and picture
 // creators is an array of Contentful creator objs. TODO implement propTypes
-export default ({creators}) => {
-  console.log("[CreatorThumbList] creators: ", creators)
+export default ({data}) => {
+  const creators = data.allContentfulCreator.edges
   return (
     <div>
       <ul style={{listStyle: `none`, float: `left`}}>
         {
           creators.map(({node}) => {
             const creator = node
-            console.log("[CreatorThumbList] map node: ", creator)
             const link = "/" + creator.slug
-            const image = creator.mainImage.file.url
-            console.log("image: ", image)
+            const image = "https:" + creator.mainImage.file.url
             return (
-              <li key={creator.id}>
-                <Link to={link}>
+              <Link to={link} key={creator.id}>
+                <li>
                   <p style={{clear: `both`, padding: `1rem 0`}}>
                     {creator.name}
                     <img src={image} alt={creator.name} style={{maxWidth: `200px`, float: `right`, padding: `0 1rem`}}/>
                   </p>
-                </Link>
-              </li>
+                </li>
+              </Link>
             )
           })
         }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,19 @@
 import React from "react"
 import Layout from "../components/layout"
-import RandomCreatorThumbList from "../components/randomCreatorThumbList"
+import CreatorThumbList from "../components/creatorThumbList"
 import { graphql } from "gatsby"
 
-export default ({data}) => (
-  <Layout>
-  <RandomCreatorThumbList data={data} />
-  </Layout>
-)
+export default ({data}) => {
+  return (
+    <Layout>
+      <CreatorThumbList data={data} />
+    </Layout>
+  )
+}
 
 export const query = graphql`
   {
-    allContentfulCreator {
+    allContentfulCreator (sort: {fields: fields___sortId}) {
       edges {
         node {
           id
