c8ee07b77ebbdbe55f063365a5a637a374c32789
David Moench <domoench@gmail.com>
2018-10-26 17:34:57 -0400

    Some experiments in static/dynamic index page
      - Modifying the state of the statically built index component
        with a button to set to first
      - Displaying categories - will later use to sort/filter

--- src/components/creatorThumbList.js
+++ src/components/creatorThumbList.js
@@ -3,8 +3,8 @@ import { Link } from "gatsby"
 
 // A list of creators, with name and picture
 // creators is an array of Contentful creator objs. TODO implement propTypes
-export default ({data}) => {
-  const creators = data.allContentfulCreator.edges
+export default (props) => {
+  const creators = props.creators
   return (
     <div>
       <ul style={{listStyle: `none`, float: `left`}}>
@@ -14,18 +14,32 @@ export default ({data}) => {
             const link = "/" + creator.slug
             const image = "https:" + creator.mainImage.file.url
             return (
-              <Link to={link} key={creator.id}>
-                <li>
+              <li key={creator.id}>
+                <Link to={link}>
                   <p style={{clear: `both`, padding: `1rem 0`}}>
                     {creator.name}
-                    <img src={image} alt={creator.name} style={{maxWidth: `200px`, float: `right`, padding: `0 1rem`}}/>
+                    <img src={image} alt={creator.name} style={{maxWidth: `300px`, float: `right`, padding: `0 1rem`}}/>
                   </p>
-                </li>
-              </Link>
+                </Link>
+                <button onClick={() => props.setToFirst(creator.id)}>Set to first</button>
+                <CategoryTags creator={creator} />
+              </li>
             )
           })
         }
       </ul>
     </div>
+  )
+}
+
+const CategoryTags = ({creator}) => {
+  return (
+    <ul>
+    {
+      creator.categories.map(({name, id}) => {
+        return <li key={id}>{name}</li>
+      })
+    }
+    </ul>
   )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,13 +3,49 @@ import Layout from "../components/layout"
 import CreatorThumbList from "../components/creatorThumbList"
 import { graphql } from "gatsby"
 
-export default ({data}) => {
-  return (
-    <Layout>
-      <CreatorThumbList data={data} />
-    </Layout>
-  )
+class Index extends React.Component {
+  constructor(props) {
+    super(props)
+    console.log("Index: constructor(). props: ", props)
+    this.setToFirst = this.setToFirst.bind(this)
+    this.state = {
+      creators: props.data.allContentfulCreator.edges,
+    }
+    console.log("Index: constructor(). creators: ", this.state.creators)
+  }
+
+  // Set the given creator as the first in the list
+  setToFirst(creatorId) {
+    let creators = this.state.creators
+    // linear search for the index of the specified creator
+    let creatorIdx = 0
+    for (let i = 0; i < creators.length; ++i) {
+      if (creatorId === creators[i].node.id) {
+        creatorIdx = i
+        break
+      }
+    }
+
+    // Swap the selected creator's position with first
+    let updated = creators.slice()
+    let tmp = updated[0]
+    updated[0] = updated[creatorIdx]
+    updated[creatorIdx] = tmp
+    this.setState({creators: updated})
+    console.log("Index: setToFirst(). creators: ", this.state.creators)
+  }
+
+  render() {
+    console.log("Index: render(). creators: ", this.state.creators)
+    return (
+      <Layout>
+        <CreatorThumbList creators={this.state.creators} setToFirst={this.setToFirst} />
+      </Layout>
+    )
+  }
 }
+
+export default Index
 
 export const query = graphql`
   {
@@ -33,6 +69,10 @@ export const query = graphql`
             file {
               url
             }
+          }
+          categories {
+            id
+            name
           }
         }
       }
