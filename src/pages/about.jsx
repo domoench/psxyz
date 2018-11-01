56a8ada9144493aecb28e36b7112705d0f077341
David Moench <domoench@gmail.com>
2018-11-01 19:47:47 -0400

    Basic styling for creator pages.

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -7,16 +7,16 @@ export default () => (
     <Grid>
       <h1>About</h1>
       <p>
-        {'A little happy sunlight shining through there. But we&apos;re not there yet, so we don&apos;t '
+        {'A little happy sunlight shining through there. But we\'re not there yet, so we don\'t '
           + 'need to worry about it. The light is your friend. Preserve it. I guess that would be considered '
           + 'a UFO. A big cotton ball in the sky.'}
       </p>
       <p>
-        {'There isn&apos;t a rule. You just practice and find out which way works best for you. Now we '
-          + 'don&apos;t want him to get lonely, so we&apos;ll give him a little friend. I started '
-          + 'painting as a hobby when I was little. I didn&apos;t know I had any talent. I believe talent is '
+        {'There isn\'t a rule. You just practice and find out which way works best for you. Now we '
+          + 'don\'t want him to get lonely, so we\'ll give him a little friend. I started '
+          + 'painting as a hobby when I was little. I didn\'t know I had any talent. I believe talent is '
           + 'just a pursued interest. Anybody can do what I do. Get tough with it, get strong. Use your '
-          + 'imagination, let it go. We don&apos;t need any guidelines or formats. All we need to do is just '
+          + 'imagination, let it go. We don\'t need any guidelines or formats. All we need to do is just '
           + 'let it flow right out of us.'}
       </p>
     </Grid>
--- src/templates/creator.jsx
+++ src/templates/creator.jsx
@@ -2,25 +2,30 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { graphql } from 'gatsby';
 import {
-  Grid, Image,
+  Grid, Image, Row, Col,
 } from 'react-bootstrap';
 import Layout from '../components/layout';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import styles from './creator.module.less';
 
 const Creator = ({ data }) => {
   const creator = data.contentfulCreator;
   return (
     <Layout>
       <Grid>
-        <h1>{creator.name}</h1>
-        <p>{data.contentfulCreator.childContentfulCreatorBioTextNode.bio}</p>
-        <ul>
-          {creator.images.map(image => (
-            <li key={image.id}>
-              <Image src={image.file.url} responsive />
-            </li>
-          ))}
-        </ul>
+        <Row className={styles.creator}>
+          <Col>
+            <h1>{creator.name}</h1>
+            <p>{data.contentfulCreator.childContentfulCreatorBioTextNode.bio}</p>
+            <ul>
+              {creator.images.map(image => (
+                <li key={image.id}>
+                  <Image src={image.file.url} responsive />
+                </li>
+              ))}
+            </ul>
+          </Col>
+        </Row>
       </Grid>
     </Layout>
   );
