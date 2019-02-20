fcba1215a690b710c6bc960c75ade61f95104f5e
David Moench <domoench@gmail.com>
2019-02-20 12:19:18 -0800

    Make splash page show on in-development site.
    Also:
    - Dependency updates
    - Animation changes: Always fade in

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,14 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Header from './Header';
+import Splash from './Splash';
 import styles from './Layout.module.less';
 
-const Layout = ({ children }) => (
-  <div className={styles.container}>
-    <Header />
-    {children}
-  </div>
-);
+const Layout = ({ children }) => {
+  if (process.env.GATSBY_IN_DEV === 'true') {
+    return <Splash />;
+  }
+  return (
+    <div className={styles.container}>
+      <Header />
+      {children}
+    </div>
+  );
+};
 
 Layout.propTypes = {
   children: PropTypes.element,
