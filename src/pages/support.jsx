fbaadb75c5ad78ded6ef5456618a574234cc6bbb
David Moench <domoench@gmail.com>
2020-02-23 18:09:28 -0500

    Preliminary donate links: Paypal + Venmo.

--- src/pages/support.jsx
+++ src/pages/support.jsx
@@ -1,16 +1,59 @@
 import React from 'react';
 import styled from 'styled-components';
+import PropTypes from 'prop-types';
 
 import Layout from '../components/Layout';
 import { Wrapper, Column } from '../components/newspaper';
-import { colors, fonts, fontSize } from '../theme';
+import { colors } from '../theme';
+import venmoQRCode from '../assets/venmo_QR.png';
+import paypalQRCode from '../assets/paypal_QR.png';
 
-const DonateLink = styled.div`
-  font-weight: 600;
-  font-family: ${fonts.sansSerif};
-  font-size: ${fontSize.body * 2}px;
-  padding: 1em 0;
+const DonateQRs = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-context: space-evenly;
 `;
+
+const DonateQR = styled.div`
+  min-width: 250px;
+  flex-basis: auto;
+  flex-grow: 1;
+  padding: 0.5em;
+
+  & h2 {
+    text-align: center;
+    padding: 0.5em 0em;
+  }
+`;
+
+const QRAnchor = ({
+  imgSrc,
+  href,
+  alt,
+  className,
+}) => (
+  <a className={className} href={href}>
+    <img src={imgSrc} alt={alt} />
+  </a>
+);
+
+QRAnchor.propTypes = {
+  imgSrc: PropTypes.string,
+  href: PropTypes.string,
+  alt: PropTypes.string,
+  className: PropTypes.string,
+};
+
+const StyledQRAnchor = styled(QRAnchor)`
+  display: inline-block;
+  width: 100%;
+  & img {
+    width: 100%;
+  }
+`;
+
+// TODO create a new Paypal donate button URL + QR code that redirect back to
+// the live PSxyz site (instead of github)
 
 const Support = () => (
   // TODO: What if a user clicks show filters on this page? Should we hide the filters button?
@@ -24,9 +67,25 @@ const Support = () => (
         <p>
           Supporting Public Service is an investment in diversifying imagemaking. Donate to us via Venmo to help support costs of hosting and maintenence for this resource.
         </p>
-        <DonateLink>
-          [TODO] DONATE HERE →
-        </DonateLink>
+        <DonateQRs>
+          <DonateQR>
+            <h2>Venmo</h2>
+            <StyledQRAnchor
+              imgSrc={venmoQRCode}
+              href="https://venmo.com/Public-Service"
+              alt="Venmo QR Code"
+            />
+          </DonateQR>
+
+          <DonateQR>
+            <h2>Paypal</h2>
+            <StyledQRAnchor
+              imgSrc={paypalQRCode}
+              href="https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=NKHMRH58PWWWE&source=url"
+              alt="Paypal QR Code"
+            />
+          </DonateQR>
+        </DonateQRs>
       </Column>
       <Column />
     </Wrapper>
