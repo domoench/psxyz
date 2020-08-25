f00541cfef50cd836caf56e5170ab11ee5304efe
David Ouyang Moench <domoench@users.noreply.github.com>
2020-08-24 21:55:20 -0400

    Header/Footer Link Styles (#35)
    * Update pill padding and colors to match designs.
    
    * Dynamically size header nav pills.
    
    - Move width calculation up to layout, so it can be shared with both ImageGrid
      and Header
    - Use width in header to determine font size based on device size
    - props-drill the location down to links to determine if they are active or not.
      There's probably a better GatsbyLink way to do this, but not sure how make it
      play nice with styled components.
    
    * Also resize footer pills based on device size.
    
    * Tweak footer pills to match header.

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -5,27 +5,33 @@ import { Wrapper, Column } from '../components/newspaper';
 import { colors } from '../theme';
 import Anchor from '../components/reusable/Anchor';
 
-const Contact = () => (
+const Contact = ({ location }) => (
   // TODO: What if a user clicks show filters on this page? Should we hide the filters button?
-  <Layout toggleFiltersDrawer={() => {}} showFilters={false}>
-    <Wrapper color={colors.blue}>
-      <Column>
-        <p>
-          To keep in-touch with all things Public Service share your e-mail with
-          us (we won’t ever sell your data or do other evil shit).
-        </p>
-        <p>
-          <Anchor
-            href="mailto:people@publicservice.com"
-            altText="people@publicservice.com"
-            underline
-          >
-            people@publicservice.com
-          </Anchor>
-        </p>
-      </Column>
-      <Column />
-    </Wrapper>
+  <Layout
+    location={location}
+    toggleFiltersDrawer={() => {}}
+    showFilters={false}
+  >
+    {(/* width */) => (
+      <Wrapper color={colors.blue}>
+        <Column>
+          <p>
+            To keep in-touch with all things Public Service share your e-mail
+            with us (we won’t ever sell your data or do other evil shit).
+          </p>
+          <p>
+            <Anchor
+              href="mailto:people@publicservice.com"
+              altText="people@publicservice.com"
+              underline
+            >
+              people@publicservice.com
+            </Anchor>
+          </p>
+        </Column>
+        <Column />
+      </Wrapper>
+    )}
   </Layout>
 );
 
